9f14ac04d86e9e7ff4a60105914f48b34c7a848f
theUniC <theunic@gmail.com>
2020-06-11 21:56:12 +0200

    Small fix

--- config/doctrine-orm_dev.php
+++ config/doctrine-orm_dev.php
@@ -4,5 +4,5 @@ declare(strict_types=1);
 
 return [
     'driver' => 'pdo_mysql',
-    'url' => 'mysql://yiidoctrine:1234Abcd@127.0.0.1:33306/yiidoctrine?serverVersion=8',
+    'url' => 'mysql://yiidoctrine:1234Abcd@mysql:3306/yiidoctrine?serverVersion=8',
 ];
